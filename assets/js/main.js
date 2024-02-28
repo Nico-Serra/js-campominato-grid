25cfcff9289cf512a604104710b10d41f549d959
Nico-Serra <nico.serra000@gmail.com>
2024-02-28 15:59:29 +0100

    add coloured cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,46 @@
 //console.log('hello world');
 
-const form = document.querySelector('form') 
+const form = document.querySelector('form')
 //console.log(button);
 
 const container = document.querySelector('.container')
 
 //- Al click del bottone genero una griglia quadrata con 10 caselle per ognuna delle 10 righe, ogni cella sarà numerata progressivamente
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
 
     e.preventDefault
-    
-    // console.log(`${markup}`);
-    for (let i = 0; i < 100 ; i++) {
 
-        const markup = `<div class="box">${i+1}</div>`
+    // console.log(`${markup}`);
+    for (let i = 0; i < 100; i++) {
+
+        const markup = `<div class="box">${i + 1}</div>`
         container.insertAdjacentHTML('beforeend', `${markup}`)
+
+    }
+    //- Al click della cella quest'ultima cambiera colore
+    const cells = document.querySelectorAll('.box')
+
+    
+
+    for(let i = 0; i < cells.length; i++) {
+
+        const elementCell = cells[i];
+        //console.log(elementCell);
+
+        elementCell.addEventListener('click', function () {
+            elementCell.classList.toggle('skyblue')
+            console.log(elementCell);
+         })
         
     }
 
-    //- Al click della cella quest'ultima cambiera colore
-    
+    //console.log(elementCell);
+
+    //console.log(cells[0]);
+
+   
+
+
 })
 
 
